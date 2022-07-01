5a11f6fa9ada87d4ce99aa42432e27edd7384119
Okinbird <okinyinka@yahoo.co.uk>
2022-07-01 15:58:02 -0700

    My sixth commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+char a;
+for (a = 'z'; a >= 'a'; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
