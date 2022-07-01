5c9fa04669546a64160fe99037e10b6b485f94c2
Okinbird <okinyinka@yahoo.co.uk>
2022-07-01 16:40:38 -0700

    My second commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int a;
+int b;
+for (a = '0'; a <= '8'; a++)
+{
+for (b = a + 1; b <= '9'; b++)
+{
+putchar(a);
+putchar(b);
+if (b < '9' || a < '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
