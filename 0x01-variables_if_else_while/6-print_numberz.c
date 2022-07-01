dd6616e9745f932aa4215adae7b24bad56ea399d
Okinbird <okinyinka@yahoo.co.uk>
2022-07-01 15:49:40 -0700

    My sixth commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
