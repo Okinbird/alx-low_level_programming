65dc149c65a743aca13cdb13e33b6e2710b16c92
Okinbird <okinyinka@yahoo.co.uk>
2022-07-05 07:49:00 -0700

    My seventh commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Eyebiokin Olayinka
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase,
+ *                      followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+  int count = 0;
+  char letter;
+
+  while (count++ <= 9)
+    {
+      for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+      _putchar('\n');
+    }
+}
