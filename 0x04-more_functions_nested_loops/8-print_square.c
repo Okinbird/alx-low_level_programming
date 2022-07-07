1bc4a3aef849b2ba20e1ee6508cca77f137181dd
Okinbird <okinyinka@yahoo.co.uk>
2022-07-07 08:59:38 -0700

    My ninth commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+/*
+ * File: 8-print_square.c
+ * Auth: Eyebiokin Olayinka
+ */
+
+#include "main.h"
+
+/**
+ * print_square - Prints a square, using the character #,
+ *                followed by a new line.
+ * @size: Size of the square.
+ * @size = 0 || <: Print a new line.
+ */
+
+void print_square(int size)
+{
+int hgt, wid;
+
+if (size > 0)
+{
+for (hgt = 0; hgt < size; hgt++)
+{
+for (wid = 0; wid < size; wid++)
+_putchar('#');
+
+if (hgt == size - 1)
+continue;
+ 
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
