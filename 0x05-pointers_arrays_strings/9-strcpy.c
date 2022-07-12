6c1336df3f133d6c394b4388b3844ea43f0ab901
Okinbird <okinyinka@yahoo.co.uk>
2022-07-12 15:55:15 -0700

    My tenth commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies a string pointed to by @str, including the 
+ * _strcpy - Copies a string pointed to by @str, including the
  *           terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
@@ -21,12 +21,11 @@ int index = 0;
 
 while (src[index])
 {
-      
+
 dest[index] = src[index];
 index++;
 
 }
-  
 return (dest);
 
 }
