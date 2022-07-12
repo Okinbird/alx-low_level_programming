c2a0bc5ebfddc6c31c52d3b97ddc3dc065c1b976
Okinbird <okinyinka@yahoo.co.uk>
2022-07-12 14:53:50 -0700

    My tenth commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,15 +7,16 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s: The string to get the length of.
  *
- * Return: The length of @str
+ * Return: The length of @s
  */
 
 int _strlen(char *s)
 {
+
 int length = 0;
-  
+
 while (*s++)
 length++;
 
