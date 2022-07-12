2a18d143ba4e2b151e7d429ee14f5e3c5638d618
Okinbird <okinyinka@yahoo.co.uk>
2022-07-12 14:56:28 -0700

    My tenth commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,14 +6,14 @@
 #include "main.h"
 
 /**
- * puts - Prints a string to stdout,
+ * _puts - Prints a string to stdout,
  *        followed by a new line.
  * @str: The string to be printed.
  */
 
 void _puts(char *str)
 {
-  
+
 while (*str)
 _putchar(*str++);
 
