c1542fd2c17177cf9d3cf69f12f34d8980f0193c
Okinbird <okinyinka@yahoo.co.uk>
2022-07-12 17:00:45 -0700

    My tenth commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,18 +15,18 @@
  * Return: A pointer to the destination string @dest.
  */
 
-char _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 
 int index = 0;
 
-while (src[index])
+while (*src[index])
 {
 
-dest[index] = src[index];
+*dest[index] = *src[index];
 index++;
 
 }
-return (dest);
+return (*dest);
 
 }
