652f65715bce98a472653e554bccae05d67c904a
Okinbird <okinyinka@yahoo.co.uk>
2022-07-14 05:26:41 -0700

    My third commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,9 +18,7 @@ if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else
-{
-if (n < 0)
+else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
@@ -28,6 +26,6 @@ else
 {
 printf("%d is zero\n", n);
 }
-}
+
 return (0);
 }
