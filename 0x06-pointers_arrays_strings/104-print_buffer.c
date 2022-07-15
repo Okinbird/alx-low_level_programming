d7a1b53e91d96e2d2571b686f55919e8103695bb
Okinbird <okinyinka@yahoo.co.uk>
2022-07-14 17:21:57 -0700

    My eleventh commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,36 +22,27 @@ printf("%08x: ", byte);
 for (index = 0; index < 10; index++)
 {
 if ((index + byte) >= size)
-printf( " ");
+printf(" ");
 
 else
 printf("%02x", *(b + index + byte));
-
 if ((index % 2) != 0 && index != 0)
 printf(" ");
 }
-
-
 for (index = 0; index < 10; index++)
 {
 if ((index + byte) >= size)
 break;
-
 else if (*(b + index + byte) >= 31 &&
 *(b + index + byte) <= 126)
 printf("%c", *(b + index + byte));
-
 else
 printf(".");
 }
-
 if (byte >= size)
 continue;
-
 printf("\n");
 }
-
 if (size <= 0)
 printf("\n");
-
 }
