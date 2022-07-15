1862d248fa1433e5809dc161009f022a4a7e14c2
Okinbird <okinyinka@yahoo.co.uk>
2022-07-14 17:17:39 -0700

    My eleventh commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -20,45 +20,37 @@ int k;
 int l;
 int m = 0;
 int n = 0;
-
-for (i = 0; n1[i]; i++);
-
-for (j = 0; n2[j]; j++);
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
 
 if (i > size_r || j > size_r)
 return (0);
 
-  
 for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
-
 n = m;
 if (i >= 0)
 n += n1[i] - '0';
 
 if (j >= 0)
 n += n2[j] - '0';
-
 if (i < 0 && j < 0 && n == 0)
 {
 break;
 }
- 
 m = n / 10;
 r[k] = n % 10 + '0';
 }
-
 r[k] = '\0';
 if (i >= 0 || j >= 0 || m)
 return (0);
-
 for (k -= 1, l = 0; l < k; k--, l++)
 {
-
 m = r[k];
-r[k] = r[1];
+r[k] = r[l];
 r[l] = m;
 }
 return (r);
-
 }
