c19bf0c5459c2f4182183db8eb4262117ad10b2c
Okinbird <okinyinka@yahoo.co.uk>
2022-07-20 10:15:01 -0700

    My fourteenth commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -31,7 +31,7 @@ int check_palindrome(char *s, int len, int index)
 if (s[index] == s[len / 2])
 return (1);
 if (s[index] == s[len - index - 1])
-  return (check_palindrome(s, len, index + 1));
+return (check_palindrome(s, len, index + 1));
 return (0);
 }
 
