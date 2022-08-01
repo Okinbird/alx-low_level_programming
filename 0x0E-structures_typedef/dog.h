1abd01ceb31434289b4399c7e7f35a30bf3a0aa0
Okinbird <okinyinka@yahoo.co.uk>
2022-08-01 15:46:29 -0700

    My nineteenth commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,18 +8,12 @@
  * @owner:  The owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 int _puchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
