e72523d040b0a5dadcb04c676db84a4bb5056076
Okinbird <okinyinka@yahoo.co.uk>
2022-08-04 11:33:05 -0700

    My twenty-one commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * sum_them_all - Returns the sum of all its parameters
  * @n:  The number of parameters passed to the function.
  * @...:  A variable number of parameters to calculate the sum of.
- * 
+ *
  * Return: If n == 0 - 0.
  *         Otherwise - The sum of all parameters.
  */
