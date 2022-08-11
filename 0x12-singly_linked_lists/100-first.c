02966ca3c65614a63a16ff6263961a49892e50de
Okinbird <okinyinka@yahoo.co.uk>
2022-08-11 09:51:44 -0700

    My twenty-three commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) first(void);
+
+/**
+ * first - Prints something before main
+ * 
+ * Return: void
+ */
+void first(void)
+{
+  printf("You're beat! and yet, you must allow,\n"
+	 "I bore my house upon my back!\n");
+}
