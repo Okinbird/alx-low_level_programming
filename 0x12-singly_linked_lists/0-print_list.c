481931e6ec65540a3d8eca795dc46c795093863d
Okinbird <okinyinka@yahoo.co.uk>
2022-08-11 06:41:26 -0700

    My twenty-three commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
-  size_t nodes = 0;
+size_t nodes = 0;
 
-  while (h != NULL)
-    {
-      if (h->str == NULL)
-	printf("[0] (nil)\n");
-      else
-	printf("[%d] %s\n", h->len, h->str);
-      nodes++;
-      h = h->next;
-    }
-  return (nodes);
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
