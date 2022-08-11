193c0272c8f00edbc4b7b1c82e32227f3ae9951b
Okinbird <okinyinka@yahoo.co.uk>
2022-08-11 09:54:37 -0700

    Betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,5 @@ void __attribute__((constructor)) first(void);
 void first(void)
 {
 printf("You're beat! and yet, you must allow,\n"
-	 "I bore my house upon my back!\n");
+"I bore my house upon my back!\n");
 }
