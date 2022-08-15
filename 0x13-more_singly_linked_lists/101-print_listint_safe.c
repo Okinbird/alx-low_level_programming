7154b1417fa3f0d2dc77a608afec62fbebcb23df
Okinbird <okinyinka@yahoo.co.uk>
2022-08-15 16:00:13 -0700

    My twenty-four commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -59,18 +59,20 @@ nodes = looped_listint_len(head);
 
 if (nodes == 0)
 {
-for (; head != NULL; nodes++){
+for (; head != NULL; nodes++)
+{
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
 }
 else
 {
-for (index = 0; index < nodes; index++){
+for (index = 0; index < nodes; index++)
+{
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
-printf("-> [%p] %d\n", (void *)head, head->n); 
+printf("-> [%p] %d\n", (void *)head, head->n);
 }
 return (nodes);
 }
