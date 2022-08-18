453dca1d4431dd1350baff9b8ae61257f9f4cd47
Okinbird <okinyinka@yahoo.co.uk>
2022-08-18 14:02:05 -0700

    My twenty-five commit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,10 +11,10 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int set;
-  
+
 if (index > (sizeof(unsigned long int) * 8 - 1))
 return (-1);
- 
+
 set = ~(1 << index);
 *n &= set;
 
