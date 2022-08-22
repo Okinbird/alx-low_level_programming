427e5d38b3ceffda465ff6045b1b124f3ecc68de
Okinbird <okinyinka@yahoo.co.uk>
2022-08-22 15:24:59 -0700

    My twenty-six commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,6 @@ if (buffer == NULL)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 exit(99);
 }
-
 return (buffer);
 }
 
@@ -63,7 +62,6 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-
 buffer = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
 r = read(from, buffer, 1024);
@@ -77,7 +75,6 @@ dprintf(STDERR_FILENO,
 free(buffer);
 exit(98);
 }
-
 w = write(to, buffer, r);
 if (to == -1 || w == -1)
 {
@@ -86,15 +83,11 @@ dprintf(STDERR_FILENO,
 free(buffer);
 exit(99);
 }
-
 r = read(from, buffer, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
-
 } while (r > 0);
-
 free(buffer);
 close_file(from);
 close_file(to);
-
 return (0);
 }
