7f08708983b873a938f6387b3a267d33afb92251
Okinbird <okinyinka@yahoo.co.uk>
2022-10-03 17:21:03 -0700

    dynamic libraries task 0 - 3

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s:  The string to check
+ * @accept: The string to check against
+ *
+ * Return: The number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+  unsigned int i, j;
+
+  for (i = 0; s[i]; i++)
+    {
+      for (j = 0; accept[j]; j++)
+	{
+	  if (s[i] == accept[j])
+	    break;
+	}
+      if (!accept[j])
+	break;
+    }
+  return (i);
+}
