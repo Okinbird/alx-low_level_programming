deffa824877f4a2a5201da52746fab3dd7c55c51
Okinbird <okinyinka@yahoo.co.uk>
2022-10-03 18:04:40 -0700

    dynamic library task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Okinbird/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so 
+wget -P /tmp/ https://github.com/Okinbird/alx-low_level_programming/raw/master/0x18-dynamic_libraries/len.so
+export LD_PRELOAD=/tmp/len.so 
