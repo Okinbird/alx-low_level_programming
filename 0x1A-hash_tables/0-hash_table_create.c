4fedee199153c5175cb2d1b4fd7e48e14063ad91
Okinbird <okinyinka@yahoo.co.uk>
2022-10-20 16:31:04 -0700

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,16 +12,19 @@ hash_table_t *hash_table;
 unsigned long int i;
 
 hash_table = malloc(sizeof(hash_table_t));
+
 if (hash_table == NULL)
-  return (NULL);
+return (NULL);
 hash_table->size = size;
 hash_table->array = malloc(size * sizeof(hash_node_t *));
+
 if (hash_table->array == NULL)
 {
-  free(hash_table);
-  return (NULL);
+free(hash_table);
+return (NULL);
 }
+
 for (i = 0; i < size; i++)
 hash_table->array[i] = NULL;
- return (hash_table);
+return (hash_table);
 }
