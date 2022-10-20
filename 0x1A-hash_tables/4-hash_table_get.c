828a04d23eced43025ad9e906879c1c708daadf8
Okinbird <okinyinka@yahoo.co.uk>
2022-10-20 16:42:23 -0700

    hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,19 +11,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int index;
-  hash_node_t *tmp;
+unsigned long int index;
+hash_node_t *tmp;
 
-  if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-      key == NULL || strlen(key) == 0)
-    return (NULL);
-  index = key_index((const unsigned char *)key, ht->size);
-  tmp = ht->array[index];
-  while (tmp != NULL)
-    {
-      if (strcmp(tmp->key, key) == 0)
-	return (tmp->value);
-      tmp = tmp->next;
-    }
-  return (NULL);
+if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+key == NULL || strlen(key) == 0)
+return (NULL);
+index = key_index((const unsigned char *)key, ht->size);
+tmp = ht->array[index];
+while (tmp != NULL)
+{
+if (strcmp(tmp->key, key) == 0)
+return (tmp->value);
+tmp = tmp->next;
+}
+return (NULL);
 }
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,24 +9,24 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-  unsigned long int i;
-  hash_node_t *tmp;
-  char flag = 0; /* 0 while no data has been printed yet */
+unsigned long int i;
+hash_node_t *tmp;
+char flag = 0; /* 0 while no data has been printed yet */
 
-  if (ht == NULL || ht->array == NULL)
-    return;
-  printf("{");
-  for (i = 0; i < ht->size; i++)
-    {
-      tmp = ht->array[i];
-      while (tmp != NULL)
-	{
-	  if (flag == 1)
-	    printf(", ");
-	  printf("'%s': '%s'", tmp->key, tmp->value);
-	  flag = 1;
-	  tmp = tmp->next;
-	}
-    }
-  printf("}\n");
+if (ht == NULL || ht->array == NULL)
+return;
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+tmp = ht->array[i];
+while (tmp != NULL)
+{
+if (flag == 1)
+printf(", ");
+printf("'%s': '%s'", tmp->key, tmp->value);
+flag = 1;
+tmp = tmp->next;
+}
+}
+printf("}\n");
 }
